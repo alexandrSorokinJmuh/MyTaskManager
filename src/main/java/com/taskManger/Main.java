3d65f3e2e130520bde1e9364efbcbf5523182b83
alexandrSorokinJmuh <80698476+alexandrSorokinJmuh@users.noreply.github.com>
2021-03-29 17:04:37 +0400

    add authorization view

--- src/main/java/com/taskManger/Main.java
+++ src/main/java/com/taskManger/Main.java
@@ -1,16 +1,109 @@
 package com.taskManger;
 
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchema;
+import com.fasterxml.jackson.module.jsonSchema.JsonSchemaGenerator;
+import com.taskManger.DataStorage.DataStorage;
+import com.taskManger.controllers.TaskController;
+import com.taskManger.controllers.UserController;
+import com.taskManger.entities.ListOfTasks;
+import com.taskManger.entities.Tasks;
 import com.taskManger.entities.User;
+import com.taskManger.entities.WatcherForTasks;
+import com.taskManger.exception.JsonValidationException;
+import com.taskManger.exception.UUIDIsNotUniqueException;
+import com.taskManger.exception.UsernameNotUniqueException;
+import com.taskManger.repositories.TaskRepository;
+import com.taskManger.repositories.UserRepository;
+import com.taskManger.services.JSONService;
+import com.taskManger.services.TaskService;
+import com.taskManger.services.UserService;
+import com.taskManger.views.AuthorizationView;
+import org.bouncycastle.util.encoders.Hex;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
 import java.util.function.Predicate;
 
 public class Main {
     public static void main(String[] args) {
-        Predicate<String> p = (String s) -> { return s == null;};
 
-        System.out.println(p.test("asd"));
+
+        DataStorage dataStorage = DataStorage.getInstance();
+        JSONService jsonService = new JSONService(dataStorage);
+        UserRepository userRepository = new UserRepository(dataStorage);
+        UserService userService = new UserService(userRepository);
+        UserController userController = new UserController(userService);
+
+        TaskRepository taskRepository = new TaskRepository(dataStorage);
+        TaskService taskService = new TaskService(taskRepository);
+        TaskController taskController = new TaskController(taskService);
+        try {
+            jsonService.import_json("src/main/resources/auto_created.json");
+        } catch (JsonValidationException e) {
+            e.printStackTrace();
+        }
+
+
+        System.out.println(dataStorage);
+        AuthorizationView authorizationView = new AuthorizationView(userController);
+
+        authorizationView.mainMenu();
+
+//        try {
+//            jsonService.import_json("G:\\javaProj\\MyTaskManager\\src\\main\\resources\\auto_created.json");
+//        } catch (JsonValidationException e) {
+//            e.printStackTrace();
+//        }
+/*
+        List<User> userList = new ArrayList<>();
+        try {
+            for (int i = 0; i < 10; i++) {
+
+                userList.add(userController.singUp("alesha" + i, "asd", "as", "sd", "123"));
+
+            }
+        } catch (UUIDIsNotUniqueException e) {
+            e.printStackTrace();
+        } catch (UsernameNotUniqueException e) {
+            e.printStackTrace();
+        }
+
+        List<Tasks> tasksList = new ArrayList<>();
+        try {
+            for (int i = 0; i < 10; i++) {
+                tasksList.add(taskController.createNewTask("task" + i, "", new Date()));
+            }
+        } catch (UUIDIsNotUniqueException e) {
+            e.printStackTrace();
+        }
+
+        List<WatcherForTasks> watcherForTasks = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            watcherForTasks.add(new WatcherForTasks(UUID.randomUUID().toString(), userList.get(i).getUuid()));
+        }
+
+        List<ListOfTasks> listOfTasks = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            listOfTasks.add(new ListOfTasks(UUID.randomUUID().toString(), userList.get(i).getUuid(), tasksList.get(i).getUuid(), "list of task" + i));
+        }
+
+
+
+        try {
+            jsonService.export_json();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println(dataStorage);
+*/
+
+//        dataStorage.setUserList(userList);
+//        dataStorage.saveUserList();
+//        System.out.println(String.class.toString());
     }
 }
---
+++ src/main/java/com/taskManger/views/AuthorizationView.java
@@ -0,0 +1,100 @@
+package com.taskManger.views;
+
+import com.taskManger.controllers.UserController;
+import com.taskManger.entities.User;
+import com.taskManger.exception.UUIDIsNotUniqueException;
+import com.taskManger.exception.UsernameNotUniqueException;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AuthorizationView {
+
+    private final int mainMenuActionCount = 3;
+    private UserController userController;
+
+    public AuthorizationView(UserController userController) {
+        this.userController = userController;
+    }
+
+    public void mainMenu() {
+        while (true) {
+            System.out.println("Choose action to do:");
+            System.out.println("1. Sign in");
+            System.out.println("2. Sign up");
+            System.out.println("3. Exit");
+
+            Scanner in = new Scanner(System.in);
+            System.out.print("Input a number: ");
+            int num = in.nextInt();
+
+            if (num >= 1 && num <= mainMenuActionCount) {
+                if (num == 1) {
+                    signIn();
+                } else if (num == 2) {
+                    signUp();
+                }
+                break;
+            } else {
+                System.out.flush();
+                System.out.println("\n\nWrong input!!!\n");
+            }
+        }
+
+
+    }
+
+    public void signIn() {
+        System.out.println("Input account username and password:");
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("username: ");
+        String username = in.next();
+        System.out.println("password: ");
+        String password = in.next();
+
+        User user = userController.singIn(username, password);
+
+        if (user == null) {
+            System.out.println("\n\nWrong username or password\n");
+            mainMenu();
+        } else {
+            return;
+        }
+    }
+
+    public void signUp() {
+        System.out.println("Input fields:");
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("username: ");
+        String username = in.next();
+
+        System.out.println("password: ");
+        String password = in.next();
+
+        System.out.println("firstName: ");
+        String firstName = in.next();
+
+        System.out.println("lastName: ");
+        String lastName = in.next();
+
+        System.out.println("phone: ");
+        String phone = in.next();
+
+
+        User user = null;
+        try {
+            user = userController.singUp(username, password, firstName, lastName, phone);
+            mainMenu();
+        } catch (UUIDIsNotUniqueException e) {
+            e.printStackTrace();
+        } catch (UsernameNotUniqueException e) {
+            System.out.println("This username is already exists");
+            mainMenu();
+        }
+
+    }
+}
